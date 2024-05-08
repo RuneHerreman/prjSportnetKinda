dab2e17c752b24b5b9a8662a93da59328d1aaff5
Lars Patrouille <lars.patrouille@sintjozefbrugge.be>
2024-05-08 22:35:15 +0200

    Begin E-mail verificatie

--- View/WWVergeten.cs
+++ View/WWVergeten.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
 
+//toegevoegd voor SendGrid mails
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+
 namespace prjSportnetKinda
 {
     public partial class WWVergeten : Form
@@ -22,10 +27,26 @@ namespace prjSportnetKinda
         {
             /*  Mail met code wordt verstuurt naar opgegeven Emailadres
                 --> geef code in 
-                correct?:    Verander wachtwoord mogelijk
+                correct?:   Verander wachtwoord mogelijk
                 incorrect?:  Error
             */
+            Execute();
             String strCode = Interaction.InputBox("Code e-mail", "Verificatie");
+        }
+
+
+        static async Task Execute()
+        {
+
+            var client = new SendGridClient("");
+            var from = new EmailAddress("zrc.development@zrc.be", "ZRC");
+            var subject = "Sending with Sendgrid is Fun";
+            var to = new EmailAddress("lars.patrouille@gmail.com", "lars");
+            var plaintextContent = "and easy to do anywhere, even with c#";
+            var htmlContent = "<Strong>and easy to do anywhere, even with c#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plaintextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
         }
     }
 }
